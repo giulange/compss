4a882ca16065c7b27dd7a88b7cb46d6893ce9eac
Giuliano <gyuliano@libero.it>
2020-10-18 18:17:57 +0200

    update build proc for compss related images used in LandSupport

--- start_armosa.sh
+++ start_armosa.sh
@@ -4,7 +4,7 @@
 
 export CLASSPATH=$CLASSPATH:/models/TFM/libs/*.jar:/models/ET/build/libs/ET.jar:/models/CROP_ARMOSA/ArmosaLauncher/target/*.jar:/models/TFM/build/libs/landsupport-TFM.jar:/usr/share/java/postgresql-jdbc4.jar
 
-compss_agent_start --hostname=localhost --log_level=debug --rest_port=46300 --classpath=${CLASSPATH} >> /logs/compss/compss.log
+compss_agent_start --hostname=localhost --log_level=debug --rest_port=46400 --classpath=${CLASSPATH} >> /logs/compss/compss.log
 
 #compss_agent_add_resources --agent_port=46300 --agent_node=localhost  --cpu=8 localhost
 
