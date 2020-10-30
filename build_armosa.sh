02c5974923e93de02a28b7f4f5f8ae28cf0c98e7
Giuliano <gyuliano@libero.it>
2020-10-30 22:50:07 +0100

    dedicated compss image build for armosa and tfm

--- build_armosa.sh
+++ build_armosa.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # save the previous image in the meanwhile:
-docker tag armosa armosa-old:old
+docker tag compss_armosa compss_armosa-old:old
 
 # remove base tags:
-docker rmi armosa
+docker rmi compss_armosa
 
 # re-build base images:
 #  > armosa
 docker build --no-cache -t compss:armosa -f Dockerfile_armosa .
-docker tag compss:armosa armosa
+docker tag compss:armosa compss_armosa
 docker rmi compss:armosa
 
--- build_tfm.sh
+++ build_tfm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # save the previous image in the meanwhile:
-docker tag tfm tfm-old:old
+docker tag compss_tfm compss_tfm-old:old
 
 # remove base tags:
-docker rmi tfm
+docker rmi compss_tfm
 
 # re-build base images:
 #  > tfm
-docker build --no-cache -t compss:latest -f Dockerfile .
-docker tag compss:latest tfm
+docker build --no-cache -t compss:latest -f Dockerfile_tfm .
+docker tag compss:latest compss_tfm
 docker rmi compss:latest
 
