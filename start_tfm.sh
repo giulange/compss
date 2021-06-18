21e9af7dffcc13dc2f6972beb6bcda7fdf89fbc1
Giuliano <gyuliano@libero.it>
2021-06-18 12:43:20 +0200

    new log bind via lsenv which simplifies the path of compss.log

--- start_tfm.sh
+++ start_tfm.sh
@@ -5,7 +5,7 @@
 export CLASSPATH=$CLASSPATH:/models/ET_TFM/build/libs/*.jar:/models/TFM/libs/*.jar:/models/ET/build/libs/ET.jar:/models/CROP_ARMOSA/ArmosaLauncher/target/*.jar:/models/TFM/build/libs/landsupport-TFM.jar:/usr/share/java/postgresql-jdbc4.jar
 uid=$(uuidgen)
 
-compss_agent_start --hostname=localhost --log_level=debug --log_dir=/logs/compss/${uid} --rest_port=46300 --classpath=${CLASSPATH}
+compss_agent_start --hostname=localhost --log_level=debug --log_dir=/logs/${uid} --rest_port=46300 --classpath=${CLASSPATH} >> /logs/compss.log
 
 compss_agent_add_resources --agent_port=46300 --agent_node=localhost  --cpu=8 localhost
 
