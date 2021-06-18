5644a8408e230ff33b17a16322adc6371fbece82
Giuliano <gyuliano@libero.it>
2021-06-18 10:54:49 +0200

    New TFM container build

--- start_tfm.sh
+++ start_tfm.sh
@@ -2,10 +2,10 @@
 
 /usr/sbin/sshd
 
-export CLASSPATH=$CLASSPATH:/models/TFM/libs/*.jar:/models/ET/build/libs/ET.jar:/models/CROP_ARMOSA/ArmosaLauncher/target/*.jar:/models/TFM/build/libs/landsupport-TFM.jar:/usr/share/java/postgresql-jdbc4.jar
+export CLASSPATH=$CLASSPATH:/models/ET_TFM/build/libs/*.jar:/models/TFM/libs/*.jar:/models/ET/build/libs/ET.jar:/models/CROP_ARMOSA/ArmosaLauncher/target/*.jar:/models/TFM/build/libs/landsupport-TFM.jar:/usr/share/java/postgresql-jdbc4.jar
 uid=$(uuidgen)
 
-compss_agent_start --hostname=localhost --log_level=debug --log_dir=/logs/compss/${uid} --rest_port=46300 --classpath=${CLASSPATH} >> /logs/compss/compss.log
+compss_agent_start --hostname=localhost --log_level=debug --log_dir=/logs/compss/${uid} --rest_port=46300 --classpath=${CLASSPATH}
 
 compss_agent_add_resources --agent_port=46300 --agent_node=localhost  --cpu=8 localhost
 
